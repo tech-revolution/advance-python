aaf1cbbb23e48ef92cd84af54595a2b5ae6294fa
Hassan <odootest90@gmail.com>
2024-07-24 15:39:07 +0500

    We add class 2 file

---
+++ class-2.py
@@ -0,0 +1,50 @@
+# import camelcase
+
+# print("Hello World")
+
+# a = camelcase.CamelCase()
+
+# demo = "hello this our class two"
+
+# print(a.hump(demo))
+
+# Varables
+
+# a = 1 #integer
+# emp_name = "Ali" #String
+# emp_name = 'Bilal'
+# b = 3.14 # float
+
+
+# a, b, c = 1 , "Ali", 2.1
+
+# print(c)
+
+# fruits = ["Apple", "Mango", "Grapes"] #list
+
+# x,y,z = fruits
+
+# print(fruits)
+
+# Global and Local
+
+# x = "Ruby"
+
+# def myfun():
+#     b = "Muskan"
+#     print(x)
+
+# myfun()
+
+name = "Kainat "
+print(len(name))
+
+print("My name is "+ name)
+
+" "
+'am'
+'''
+    Multiline
+    Doc String
+    
+'''
