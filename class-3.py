3a1f7b34a66b4968c8a759e62987378cd7121da4
Hassan <odootest90@gmail.com>
2024-07-25 15:37:48 +0500

    Added class 3 Data Types and Strings

---
+++ class-3.py
@@ -0,0 +1,81 @@
+'''
+Data Types
+1. str String
+2. int, float, complex Numeric Datatype
+3. list, tuple, range Sequence Types
+4. dict
+5. set, frozenset
+6. bool
+7. None
+'''
+# x = None
+# print(type(x))
+# x = frozenset({"Hello", "World"})
+# print(type(x))
+
+# std = {
+#     "name": "Ali",
+#     "age": 34
+# }
+# print(type(std))
+# x = range(5)
+# print(type(x))
+
+# t1 = (23,56)
+# print(t1[0])
+
+
+# Student = ["FirstName", "LastName", "email", 24]
+# print(Student[1])
+# print(type(Student))
+
+
+# str1 = "Hello"
+# str2 = 'World'
+
+# int1 = 23
+# ft1 = 23.5j
+# com1 = 12j
+
+# print(type(ft1))
+# print(ft1)
+
+# String
+
+# a = "Hello"
+# a = 'Hello'
+# a = """
+# 1. str String
+# 2. int, float, complex Numeric Datatype
+# 3. list, tuple, range Sequence Types
+# 4. dict
+# 5. set, frozenset
+# 6. bool
+# 7. None
+
+# """
+# print(a)
+
+
+# print(x[4])
+# i = 4
+# for i in x:
+#     print(i)
+# print(len(x))
+# print("file" in x)
+# print("file" not in x)
+
+# if "file" not in x:
+#     print("No this word is available")
+
+x = "Hello, World, this, is python file"
+# print(x[-10:-4:])
+# print(x.upper())
+# print(x.lower())
+# print(x)
+# print(x.strip())
+# print(x.replace("World", "u"))
+a = x.split(",")
+print(a[3])
+
+
