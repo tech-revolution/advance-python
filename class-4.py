5323dbd7f5441393a3835397897b071e9123c06f
Hassan <odootest90@gmail.com>
2024-07-26 16:02:25 +0500

    Today lec 4 on Strings and Strings Methods

---
+++ class-4.py
@@ -0,0 +1,21 @@
+# a = "Hello "
+# b = "World"
+# c = 'Ali'
+# x = f"Hello My name is {c} and hi"
+# print(x)
+# sort key for comments in Vscode ctrl+/
+
+# Escape Characters
+# b = "C:\\Users\\Office\\Desktop\\New folder\\class-2.py"
+
+# a = "Hello \nWorld"
+# print(a[7])
+d = "45dfdf"
+# x = f"Hello hi apple, {d:.2f} this class  apple"
+a = "apple"
+print(d.isalnum())
+# y = x.count("apple", 12)
+# print(y)
+
+# b = a.center(20, "a")
+# print(b)
