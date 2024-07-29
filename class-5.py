f3ebd87161748ba423545f5c93f80fdf0f2e2a11
Hassan <odootest90@gmail.com>
2024-07-29 15:07:08 +0500

    Added class 5 topic is type casting keywords and arithmetic operators

---
+++ class-5.py
@@ -0,0 +1,45 @@
+print("Class Five")
+
+# print = 23
+# print(print)
+
+# Integer Function
+# age = int(input("Enter your Age: \n"))
+
+
+# print(type(name))
+# print("You entered", age)
+
+# temp = str(6)
+# temp = int(4.555)
+# temp = float(5)
+# print(temp)
+
+# ip = 192.168.10.1
+
+
+# Bool
+# fname = True
+# lname = False
+# print(5<6)
+
+# Operators in Python
+
+# 1. Arithmetic Operator
+
+# +, -, /, *, %, **, //
+a = 17
+b = 3
+# print(a+b)
+# print(a-b)
+# print(a/b)
+# print(a*b)
+# print(a%b)
+# print(a//b)
+
+
+
+
+
+
+
