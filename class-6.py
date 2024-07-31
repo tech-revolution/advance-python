c25254942516ff86af5f058ba8fd30f4a6a7a380
Hassan <odootest90@gmail.com>
2024-07-31 15:37:44 +0500

    Added class 7 Topic is List

---
+++ class-6.py
@@ -0,0 +1,47 @@
+print("Hello World")
+# Arithmetic Operators  ctrl+/ for comments
+'''
+This is multiline comments
+'''
+
+# +, -, *, /, %, **, //
+
+# Assignment Operators
+
+# =, +=, -=, *=, /=, %=,**=,//=
+# a = 3000
+# b = 20
+# print(a)
+# a+=b
+# print("Total bill ",a)
+# name = "Salman"
+
+# print(name)
+
+# Comparision Operators
+# ==, !=, >, <, >=, <=
+
+# a = 4
+# b = 9
+# # print(a <= b)
+# # Logical Operators
+# # and &&, or ||, not !
+# if not(a<5 or b<6):
+#     print("Yes you are right")
+# else:
+#     print("No")
+
+# Identity Operators
+# is, is not
+
+# a = 4
+# b = 4
+
+# print(a is not b)
+
+
+
+
+
+
+
---
+++ class-7.py
@@ -0,0 +1,57 @@
+# Membership Operators
+
+# in, not in
+list1 = ["Apple", "Banana", "Grapes"]
+a, b, c = list1
+# x = "Apple""Banana""Grapes"
+# print("Apple" not in list1)
+# print(b)
+
+# 0 = 0000000000000
+# 1 = 0000000000001
+# 2 = 0000000000010
+# 3 = 0000000000011
+# 4 = 0000000000100
+# 5 = 0000000000101
+# 6 = 0000000000110
+# 7 = 0000000000111
+
+# 6 = 0000000000110
+# 3 = 0000000000011
+# 2 = 0000000000111
+
+# print(6 | 3)
+
+# List / Array
+
+
+# print(list12[0])
+# print(list12[1])
+# print(list12[2])
+# print(list12[3])
+# a = 20
+
+# if a in list12:
+#     print("Yes A is available")
+
+# print("print list using loop")
+# for i in list12:
+#     print(i)
+
+# i++
+# i = i+1
+# for (int i=0;  i<=4; i++){
+#     console.log(list12[i])
+# }
+# list12 = ["Ali", 23, 3.5, True, "Hello", "Apple", "Banana"]
+
+# list12[2:6] = [2.7,"Water"]
+
+# print(list12)
+# list12.insert(2, 3.2)
+# print(list12)
+
+newlist0 = []
+newlist = list(("Ali", 23, 3.5, True, "Hello", "Apple", "Banana"))
+
+print(type(newlist))
