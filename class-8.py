5406e8ffdec42bf55c787454d6e064d6de9d1e10
Hassan <odootest90@gmail.com>
2024-08-02 15:53:51 +0500

    Added class 8 complete list

---
+++ class-8.py
@@ -0,0 +1,52 @@
+# in_str = "hellohello"
+
+# f_char = in_str[0]
+# print(f_char)
+
+
+# m_str = in_str.replace(f_char, "@")
+# print(m_str)
+
+
+# List Add
+
+# list1 = [4,3,6,9,2,0,1]
+# list1.sort(reverse=True)
+# list1.reverse()
+# print(list1)
+# newlist = []
+# for i in list1:
+#     newlist.append(i)
+
+# print(newlist)
+# newlist.extend(list1)
+# print(newlist)
+# tu1 =( "Bilal", "Raza", "Hassan", "Burhan")
+# list1.pop(1)
+# print(list1)
+# del list1
+# print(list1)
+# list1.clear()
+# print(list1)
+# list1.extend(tu1)
+# # print(type(list1))
+# print(list1)
+# list1.remove("Ali")
+# print(list1)
+
+# list1[3] = "Nimra"
+# list1.append("Bilal")
+# list1.insert(2, "Kaif")
+# print(list1 + list2)
+
+# list1.extend(list2)
+# print(list1)
+# list2.extend(list1)
+# print(list2)
+
+lis1 = ["Bilal", "Raza", "Hassan", "Burhan"]
+# newlis = lis1.copy()
+# print(newlis)
+newlis = list(lis1)
+print(newlis)
+print(newlis[::])
