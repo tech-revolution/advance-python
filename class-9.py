6d7c8f71fb064824cdfc72bf941a1d40fea711e2
Hassan <odootest90@gmail.com>
2024-08-05 16:20:06 +0500

    Added class 9 Tuples and Set

---
+++ class-9.py
@@ -0,0 +1,23 @@
+# Tuples
+# tu1 = (23,34,56,12,67,9,45)
+# tu2 = (23,34,56,12,67,9,45)
+# list1 = list(tu1)
+# list1[1] = 22
+# lis1 = ["ali", "nimra", "anmol", "murtaza","anmol", "murtaza"]
+# a, *b, c, d = lis1
+# # tu1 = tuple(list1)
+# print(a,b,c,d)
+# del tu1
+# print(tu1.count(34))
+# print(tu1.index(34))
+
+# for x in range(len(tu1)):
+#     print(f"index is {x} and value is {tu1[x]}")
+
+# Set
+# set1 = {"ali", "bilal", "salman", 23, "burhan"}
+# print(set1)
+# new_set = set(("Ali", "Bilal"))
+# new_set.add(34)
+# print(new_set.pop)
+    
