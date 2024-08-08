724c1089a53034caa011a1a09ce3230ecafcfe00
Hassan <odootest90@gmail.com>
2024-08-08 16:00:01 +0500

    Added class 12 recap and loops

---
+++ class-12.py
@@ -0,0 +1,89 @@
+# Python is case sensivtive
+'''
+Data types
+String 
+name = "Ali" / 'Ali'
+Name = "Ali" / 'Ali'
+integer
+age  = 34 
+float
+price  = 2.5
+bool
+var1 = True or False
+if 6>5:
+    True
+List
+ls1 = ["ali", age, 34, True]
+tuple
+tu1 = ("Ali","Ali")
+Dictionary
+std1 = {
+    "name":"Bilal",
+    "age":34,
+    "lname": "Bilal",
+    "lname": "Salman"
+}
+Sets
+set1 = {"Ali", "Ali"} duplicate not allowed but changeable
+frozenset
+set1 = {"Ali", "Ali"} duplicate not allowed and not changeable
+Typecasting
+tu1 =("Ali","Ali")
+ls1 = list(tu1)
+ls1 = ["Ali","Ali"]
+ls1[1] = "Bilal"
+ls1 = ["Ali","Bilal"]
+tu1 = tuple(ls1)
+Operators
+Assignment, Comparision, Arithmetic, logical,
+identity, membership 
+Conditional Statements
+if else elif
+
+Todays Topic 
+Loops
+for loop
+while loop
+
+'''
+# print("1")
+# print("2")
+# print("3")
+# print("4")
+# print("5")
+i = 0
+# while i <= 10:
+#     print(i)
+#     i += 1 # i++, i=i+1
+
+'''
+0
+0+1 = 1
+1+1 = 2
+2+1 = 3
+for (i = 0; i<10; i++){
+
+
+}
+while i<10{
+print
+i++
+}
+
+'''
+while i<=8:
+    print("hhhh",i)
+    i+=1
+    print(i)
+    # if i==3:
+    #     # break
+    #     continue
+    
+else:
+    print("i is end")
+
+    
+
+
+
+
